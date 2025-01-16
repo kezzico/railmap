e332725397f4e10fbb868bc9d892e110424ad683
Lee Irvine <lee@kezzi.co>
2025-01-16 10:43:57 -0800

    add map parameter to RailMapWidget

--- lib/root_widget.dart
+++ lib/root_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:railmap/railmap.dart';
 import 'package:railmap/railmap_widget.dart';
+import 'package:railmap/railmap_generated.dart';
 
 import 'package:railmap/root_widget_state.dart';
 
@@ -40,12 +42,14 @@ class _TopWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screensize = MediaQuery.sizeOf(context);
 
+    RailMap map = generatedRailmap();
+
     return Positioned(
       top: 0,
       left: 0,
       right: 0,
       height: height,
-      child: RailMapWidget(size: Size(screensize.width, height)),
+      child: RailMapWidget(size: Size(screensize.width, height), map: map),
     );
   }
 }
